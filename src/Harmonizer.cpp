df4b30c6b050e9c0bab7213e7bd4f1f1d40fc5a5
Matthew <matthewtytel@gmail.com>
2011-10-21 11:09:57 -0700

    Harmonizer class initial commit

---
+++ src/Harmonizer.cpp
@@ -0,0 +1,50 @@
+/* 
+ * Copyright 2011 Matthew Tytel
+ *
+ * dfex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dfex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dfex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Harmonizer.h"
+
+using namespace rapidxml;
+using namespace std;
+
+Class Harmonizer::cls(string("Harmonizer"), newInstance);
+
+void Harmonizer::process(const sample_t* in, sample_t* out, int num) {
+
+    sample_t root[num];
+    mRoot->process(in, root, num);
+
+    for (int i = 0; i < num; i++)
+        out[i] = root[i] / round(in[i]);
+
+    postProcess(in, out, num);
+}
+
+xml_node<> &Harmonizer::read(xml_node<> &inode) {
+    
+    Effect::read(inode);
+
+    free(mRoot);
+    mRoot = Processor::tryReadProcessor(inode, "root", DEFAULTROOT);
+
+    return inode;
+}
+
+xml_node<> &Harmonizer::write(xml_node<> &onode) const {
+    
+    return onode;
+}
+
---
+++ src/Harmonizer.h
@@ -0,0 +1,48 @@
+/* 
+ * Copyright 2011 Matthew Tytel
+ *
+ * dfex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dfex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dfex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HARMONIZER_H
+#define HARMONIZER_H
+
+#include <stdlib.h>
+#include <math.h>
+#include "Effect.h"
+
+#define DEFAULTROOT 10000
+
+class Harmonizer : public Effect {
+public:
+
+    Harmonizer() : Effect::Effect() { 
+        mRoot =  new Constant(DEFAULTROOT);
+    }
+
+    const Class *getClass() const { return &cls; }
+    static Object *newInstance() { return new Harmonizer(); }
+
+    void process(const sample_t* in, sample_t* out, int num);
+
+protected:
+
+    static Class cls;
+    Processor *mRoot;
+
+    virtual rapidxml::xml_node<> &read(rapidxml::xml_node<> &);
+    virtual rapidxml::xml_node<> &write(rapidxml::xml_node<> &) const;
+};
+
+#endif
