bfc73c5d4406afb0a4a0f6977ec4c0788eb04cb9
Matthew <matthewtytel@gmail.com>
2011-11-06 16:03:22 -0800

    Files need to be added

---
+++ src/Memory.cpp
@@ -0,0 +1,33 @@
+/* 
+ * Copyright 2011 Matthew Tytel
+ *
+ * dfex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dfex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dfex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Memory.h"
+
+void Memory::storeSamples(const sample_t *in, int num) {
+
+    for (int i = 0; i < num; i++) {
+        mMemory[mOffset] = mMemory[mOffset + MAXMEMORY] =
+         mMemory[mOffset + MAXMEMORY * 2] = in[i];
+        mOffset = (mOffset + 1) % MAXMEMORY;
+    }
+}
+
+const sample_t* Memory::getPastSamples(int num) {
+    
+    int index = (MAXMEMORY + mOffset - num) % MAXMEMORY;
+    return &mMemory[MAXMEMORY + index];
+}
---
+++ src/Memory.h
@@ -0,0 +1,45 @@
+/* 
+ * Copyright 2011 Matthew Tytel
+ *
+ * dfex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dfex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dfex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <string.h>
+
+#include "Setup.h"
+
+#define MAXMEMORY 4800000
+
+class Memory {
+public:
+
+    Memory() : mOffset(0) {
+        memset(mMemory, 0, MAXMEMORY * 3 * sizeof(sample_t));
+    }
+
+    virtual ~Memory() { }
+
+    virtual void storeSamples(const sample_t *in, int num);
+    const sample_t* getPastSamples(int num);
+
+protected:
+
+    sample_t mMemory[MAXMEMORY * 3];
+    long mOffset;
+};
+
+#endif
